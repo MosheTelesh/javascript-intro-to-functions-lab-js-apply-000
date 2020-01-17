cf49bc60f2b3d8ab931aa216f69565c0f33fd923
Moshe Telesh <mtelesh30@gmail.com>
2020-01-17 01:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){console.log("hello")
 function sayHiToGrandma(string){
   if(string===string.toLowerrCase){return
   "I can't hear you!"}
-  else{return "YES INDEED!"}
+  else{return "YES INDEED!"}}
 
 
 
