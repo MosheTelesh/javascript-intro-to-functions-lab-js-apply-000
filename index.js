e7b25965c634a050e0e6e1ff016e228a31c1627a
Moshe Telesh <mtelesh30@gmail.com>
2020-01-17 01:18:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function logWhisper(string){console.log("hello")
 }
 
 function sayHiToGrandma(string){
-  if(string===string.toLowerCase()){
+  if(string==="I love you, Grandma."){return "I love you,too."}
+  else if(string===string.toLowerCase()){
     return "I can't hear you!"}
   else if(string===string.toUpperCase()){return "YES INDEED!"}}
 
