0e38344c07af1d11ad56d33438280d858e8aedeb
Moshe Telesh <mtelesh30@gmail.com>
2020-01-17 00:22:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+string="hello"
+function shout(string)
+{
+  return "hello".toUpperCase()
+}
+function whisper(string){return "hello"
+  
+}
+function logShout(string){                              
+  console.log("HELLO")
+
+}
+function logWhisper(string){console.log("hello") 
+}
+function sayHiToGrandma(string){
+  return "I can't hear you"
+}
+  
+}
